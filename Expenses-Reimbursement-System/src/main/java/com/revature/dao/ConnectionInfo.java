de7b00f8957853fb47198638f3c8c5bffd5101c9
Ezequiel Flores <90111141+FloresEzequiel@users.noreply.github.com>
2021-09-09 19:03:29 -0400

    Update ConnectionInfo.java

--- Expenses-Reimbursement-System/src/main/java/com/revature/dao/ConnectionInfo.java
+++ Expenses-Reimbursement-System/src/main/java/com/revature/dao/ConnectionInfo.java
@@ -4,8 +4,8 @@ package com.revature.dao;
  * Stores the information necessary to connect to the database
  */
 public class ConnectionInfo {
-    public static String url="jdbc:postgresql://database-1.chn9c5opufwq.us-east-2.rds.amazonaws.com/reimbursement_db";
-    public static String username="postgres";
-    public static String password="p4ssw0rd";
+    public static String url="";
+    public static String username="";
+    public static String password="";
 
 }
